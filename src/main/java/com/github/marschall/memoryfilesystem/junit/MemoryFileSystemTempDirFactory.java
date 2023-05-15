d02829caaab7fe6abe55eeb878ab38f22a62de25
Philippe Marschall <philippe.marschall@gmail.com>
2023-05-15 19:48:13 +0200

    look up annotation

--- src/main/java/com/github/marschall/memoryfilesystem/junit/MemoryFileSystemTempDirFactory.java
+++ src/main/java/com/github/marschall/memoryfilesystem/junit/MemoryFileSystemTempDirFactory.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.io.TempDirFactory;
+import org.junit.platform.commons.util.AnnotationUtils;
 
 import com.github.marschall.memoryfilesystem.MemoryFileSystemBuilder;
 
@@ -23,6 +25,8 @@ public final class MemoryFileSystemTempDirFactory implements TempDirFactory {
 
   @Override
   public Path createTempDirectory(ExtensionContext context) throws Exception {
+    Optional<MemoryTempDirType> type = context.getElement()
+          .flatMap(element -> AnnotationUtils.findAnnotation(element, MemoryTempDirType.class));
     if (this.fileSystem == null) {
       this.fileSystem = MemoryFileSystemBuilder.newEmpty().build();
     }
