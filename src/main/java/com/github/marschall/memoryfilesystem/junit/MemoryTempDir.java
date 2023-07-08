90033f6dc2b9211247d78bff616bf189f749c2a0
Philippe Marschall <philippe.marschall@gmail.com>
2023-07-08 13:36:19 +0200

    update to M1

--- src/main/java/com/github/marschall/memoryfilesystem/junit/MemoryTempDir.java
+++ src/main/java/com/github/marschall/memoryfilesystem/junit/MemoryTempDir.java
@@ -3,11 +3,9 @@ package com.github.marschall.memoryfilesystem.junit;
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -16,7 +14,7 @@ import org.junit.jupiter.api.io.TempDir;
 @Documented
 @Retention(RUNTIME)
 @Target({FIELD, PARAMETER, ANNOTATION_TYPE})
-//@TempDir(factory = MemoryFileSystemTempDirFactory.class)
+@TempDir(factory = MemoryFileSystemTempDirFactory.class)
 public @interface MemoryTempDir {
   
 }
